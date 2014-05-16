c18b1416f4ce66809a80604ded41e32bf6a2a320
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-05-16 14:03:41 +0200

    Proximity sensor plugin skeleton

---
+++ src/ios/CDVProximity.m
@@ -0,0 +1,82 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+#import "CDVProximity.h"
+
+@interface CDVProximity () {}
+@end
+
+@implementation CDVProximity
+
+@synthesize callbackId, status;
+
+- (CDVProximity*)init
+{
+    self = [super init];
+    if (self) {
+        self.callbackId = nil;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [self stop:nil];
+}
+
+- (void)getProximityStatus:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool: status];
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)start:(CDVInvokedUrlCommand*)command
+{
+    BOOL proximityAvailable = [[UIDevice currentDevice] setProximityMonitoringEnabled:YES];
+
+    CDVPluginResult* pluginResult = nil;
+    if (proximityAvailable){
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sensorStateChange:)
+                                                     name:@"UIDeviceProximityStateDidChangeNotification" object:nil];
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+}
+
+- (void)onReset
+{
+    [self stop:nil];
+}
+
+- (void)stop:(CDVInvokedUrlCommand*)command
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)sensorStateChange:(NSNotificationCenter *)notification
+{
+    self.status = [UIDevice currentDevice] proximityState]
+}
+
+@end
